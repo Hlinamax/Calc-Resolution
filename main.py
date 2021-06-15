813ce040eecfe94630df70d877c122af109bb752
hlina <rOxWJvmjva6>
2021-06-16 01:25:20 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print('Введите соотношение сторон')
+hor = int(input())
+vert = int(input())
+print('Введите разрешение по Вертикали')
+res = int(input())
+dp = res / vert
+total = dp * hor
+print('Разрешение по Горизонтале ')
+print(total)
+
+
+
+
